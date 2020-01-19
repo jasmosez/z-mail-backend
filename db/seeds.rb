440aaef903549d1e40906f9e79c57f5d8af2c1b4
jasmosez <jas.schaffer@gmail.com>
2020-01-18 20:15:50 -0500

    through friday

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "zman@zmail.com")
+
+
+
+
+
+# HELPER METHODS
+
+# iterates through all messages and builds our dataObj 
+    # per message
+    # determine date
+    # determine in or out
+# determine if message was sent or received
+# determine if message is currently in the inbox
+# convert date from long format to x-axis label format
+# extracts display arrays from dataObj
